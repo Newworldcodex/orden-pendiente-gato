732ba4a9db002db1cfd57db64a156e3bf25b28fd
Newworldcodex <admin.zoho@newworldcol.com>
2026-07-26 18:49:52 -0500

    Add routes for pendiente and gracias pages

--- server.js
+++ server.js
@@ -4,13 +4,20 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get(['/', '/pendiente'], (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/gracias', (req, res) => {
+  res.sendFile(path.join(__dirname, 'gracias.html'));
+});
+
 app.use(express.static(path.join(__dirname)));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.redirect('/pendiente');
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
